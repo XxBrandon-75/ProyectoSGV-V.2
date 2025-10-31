60a929ca2f9f656b0dffcc9aefd43ad87e0020f3
Daesrock <PykeWasHere@outlook.com>
2025-10-31 14:15:03 -0600

    nuevos botones para el panel de administracion

--- views/layout/perfil-menu.php
+++ views/layout/perfil-menu.php
@@ -10,8 +10,23 @@
     <h3>Panel de administración</h3>
     <ul class="perfil-menu">
         <li class="<?= ($paginaActual === 'perfil') ? 'activo' : '' ?>">
-            <a href="<?= $base_url ?>index.php?controller=home&action=perfil">
+            <a href="<?= $base_url ?>index.php?controller=home&action=perfil<?= isset($datosUsuario['VoluntarioID']) ? '&id=' . $datosUsuario['VoluntarioID'] : '' ?>">
                 <i class="fa-solid fa-id-card"></i> Mi perfil
+            </a>
+        </li>
+        <li class="<?= ($paginaActual === 'especialidades') ? 'activo' : '' ?>">
+            <a href="<?= $base_url ?>index.php?controller=home&action=especialidades<?= isset($datosUsuario['VoluntarioID']) ? '&id=' . $datosUsuario['VoluntarioID'] : '' ?>">
+                <i class="fa-solid fa-graduation-cap"></i> Especialidades
+            </a>
+        </li>
+        <li class="<?= ($paginaActual === 'tramites') ? 'activo' : '' ?>">
+            <a href="<?= $base_url ?>index.php?controller=home&action=tramites<?= isset($datosUsuario['VoluntarioID']) ? '&id=' . $datosUsuario['VoluntarioID'] : '' ?>">
+                <i class="fa-solid fa-file-alt"></i> Trámites
+            </a>
+        </li>
+        <li class="<?= ($paginaActual === 'documentacion') ? 'activo' : '' ?>">
+            <a href="<?= $base_url ?>index.php?controller=home&action=documentacion<?= isset($datosUsuario['VoluntarioID']) ? '&id=' . $datosUsuario['VoluntarioID'] : '' ?>">
+                <i class="fa-solid fa-folder-open"></i> Documentos
             </a>
         </li>
         <?php if ($puedeVerVoluntarios): ?>
