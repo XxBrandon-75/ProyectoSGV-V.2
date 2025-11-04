31ea55af9f1691869695275a0d8211bdb67614db
Daesrock <PykeWasHere@outlook.com>
2025-11-04 03:27:14 -0600

    ico jiji

--- views/layout/header.php
+++ views/layout/header.php
@@ -27,6 +27,10 @@ $base_url = $protocol . $host . ($script != '/' ? $script : '') . '/';
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $titulo_pagina; ?></title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="<?php echo $base_url; ?>OIP.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo $base_url; ?>OIP.ico">
+
 
     <link rel="stylesheet" href="<?php echo $base_url; ?>public/css/g.style.css" />
 
